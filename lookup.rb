2a67dd6f99e47b05198218b2058540a694a67b1a
PranaySaraf <37924674+PranaySaraf@users.noreply.github.com>
2021-10-01 16:06:59 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -1,12 +1,21 @@
 def get_command_line_argument
+  # ARGV is an array that Ruby defines for us,
+  # which contains all the arguments we passed to it
+  # when invoking the script from the command line.
+  # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
   if ARGV.empty?
     puts "Usage: ruby lookup.rb <domain>"
     exit
   end
   ARGV.first
 end
+
+# `domain` contains the domain name we have to look up.
 domain = get_command_line_argument
 
+# File.readlines reads a file and returns an
+# array of string, where each element is a line
+# https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
 
 def parse_dns(raw)
@@ -25,7 +34,7 @@ def resolve(dns_records, lookup_chain, domain)
   matched_dns = dns_records[domain]
 
   if (!matched_dns)
-    lookup_chain << "Error: Record not found for " + domain
+    lookup_chain << "Record not found for " + domain
     return lookup_chain
   end
 
@@ -40,7 +49,7 @@ def resolve(dns_records, lookup_chain, domain)
     return lookup_chain
   end
 
-  lookup_chain << "Invalid record type for " + domain
+  lookup_chain << "Invalid record type " + domain
   return lookup_chain
 end
 
